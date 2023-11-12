bfe369fa6d455f93219fb2a82348993aee5710ef
junseok-99 <angec5645@naver.com>
2023-11-12 23:22:02 +0900

    refactor: 에러 메시지 수정

--- src/main/java/christmas/util/validation/ValidationDateOfVisit.java
+++ src/main/java/christmas/util/validation/ValidationDateOfVisit.java
@@ -6,8 +6,7 @@ public class ValidationDateOfVisit {
     private static final String EMPTY_STRING = "";
     private static final Integer MIN_DAY = 1;
     private static final Integer MAX_DAY = 31;
-    private static final String NOT_NUMERIC_MESSAGE = "[ERROR] 숫자만 입력해주세요!";
-    private static final String INVALID_RANGE_MESSAGE = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
+    private static final String INVALID_MESSAGE = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
 
     private ValidationDateOfVisit() {
 
@@ -21,14 +20,14 @@ public class ValidationDateOfVisit {
     public static void validateNumeric(String dateOfVisitString) {
         String replacedDateOfVisit = dateOfVisitString.replaceAll(NUMBER_REGEX, EMPTY_STRING);
         if (replacedDateOfVisit.isEmpty() == false) {
-            throw new IllegalArgumentException(NOT_NUMERIC_MESSAGE);
+            throw new IllegalArgumentException(INVALID_MESSAGE);
         }
     }
 
     public static void validateDayRange(String dateOfVisitString) {
         Integer dateOfVisit = Integer.parseInt(dateOfVisitString);
         if (dateOfVisit < MIN_DAY || dateOfVisit > MAX_DAY) {
-            throw new IllegalArgumentException(INVALID_RANGE_MESSAGE);
+            throw new IllegalArgumentException(INVALID_MESSAGE);
         }
     }
 }
