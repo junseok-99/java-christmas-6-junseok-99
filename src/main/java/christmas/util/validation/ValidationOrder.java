16306e710c2660f678a65ce1fc0c5c745f4e7814
junseok-99 <angec5645@naver.com>
2023-11-12 22:15:51 +0900

    feat: 메뉴의 개수가 20개를 초과하는지 검증하는 기능 추가

--- src/main/java/christmas/util/validation/ValidationOrder.java
+++ src/main/java/christmas/util/validation/ValidationOrder.java
@@ -11,8 +11,13 @@ public class ValidationOrder {
 
     private static final Set<String> menuSet = new HashSet<String>();
     private static final Integer FORM_SIZE = 2;
+    private static final Integer ZERO = 0;
     private static final Integer MIN_MENU_COUNT = 1;
+    private static final Integer MAX_MENU_COUNT = 20;
     private static final String INVALID_MESSAGE = "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.";
+    private static final String INVALID_COUNT_SUM_MESSAGE = "[ERROR] 메뉴는 한 번에 최대 20개까지만 주문할 수 있습니다.";
+
+    private static Integer countSum = ZERO;
 
     private ValidationOrder() {
 
@@ -25,6 +30,7 @@ public class ValidationOrder {
                     validateForm(orderString);
                     validateMenuCount(orderString);
                     validateDuplicateMenu(orderString);
+                    validateMenuCountSum(orderString);
         });
     }
 
@@ -36,24 +42,40 @@ public class ValidationOrder {
     }
 
     public static void validateMenuCount(String orderString) {
-        String menuCountString = Stream.of(orderString.split(SplitTypes.MENU_REGEX, SplitTypes.REMOVE_SPACE_NUMBER))
-                .toList()
-                .get(OrderIndices.COUNT.getIndex());
-        Integer menuCount = toInteger(menuCountString);
+        Integer menuCount = getMenuCount(orderString);
         if (menuCount < MIN_MENU_COUNT) {
             throw new IllegalArgumentException(INVALID_MESSAGE);
         }
     }
 
     public static void validateDuplicateMenu(String orderString) {
-        String menuString = Stream.of(orderString.split(SplitTypes.MENU_REGEX, SplitTypes.REMOVE_SPACE_NUMBER))
-                .toList()
-                .get(OrderIndices.MENU.getIndex());
+        String menuString = getMenu(orderString);
         if (menuSet.contains(menuString)) {
-            menuSet.clear();
+            initReferences();
             throw new IllegalArgumentException(INVALID_MESSAGE);
         }
         menuSet.add(menuString);
+    }
+
+    public static void validateMenuCountSum(String orderString) {
+        countSum += getMenuCount(orderString);
+        if (countSum > MAX_MENU_COUNT) {
+            initReferences();
+            throw new IllegalArgumentException(INVALID_COUNT_SUM_MESSAGE);
+        }
+    }
+
+    private static Integer getMenuCount(String orderString) {
+        String menuCountString = Stream.of(orderString.split(SplitTypes.MENU_REGEX, SplitTypes.REMOVE_SPACE_NUMBER))
+                .toList()
+                .get(OrderIndices.COUNT.getIndex());
+        return toInteger(menuCountString);
+    }
+
+    private static String getMenu(String orderString) {
+        return Stream.of(orderString.split(SplitTypes.MENU_REGEX, SplitTypes.REMOVE_SPACE_NUMBER))
+                .toList()
+                .get(OrderIndices.MENU.getIndex());
     }
 
     private static Integer toInteger(String menuCountString) {
@@ -63,5 +85,10 @@ public class ValidationOrder {
         } catch (NumberFormatException notNumericException) {
             throw new IllegalArgumentException(INVALID_MESSAGE);
         }
+    }
+
+    private static void initReferences() {
+        menuSet.clear();
+        countSum = ZERO;
     }
 }
