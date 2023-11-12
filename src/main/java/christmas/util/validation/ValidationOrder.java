b662a70ee9882372e1e6fc335db012feb367a226
junseok-99 <angec5645@naver.com>
2023-11-12 21:15:08 +0900

    feat: 중복 메뉴를 입력했는지 검증하는 기능 추가

--- src/main/java/christmas/util/validation/ValidationOrder.java
+++ src/main/java/christmas/util/validation/ValidationOrder.java
@@ -1,16 +1,17 @@
 package christmas.util.validation;
 
-import java.util.List;
+import christmas.util.OrderIndices;
+import christmas.util.SplitTypes;
+
+import java.util.HashSet;
+import java.util.Set;
 import java.util.stream.Stream;
 
 public class ValidationOrder {
 
+    private static final Set<String> menuSet = new HashSet<String>();
     private static final Integer FORM_SIZE = 2;
-    private static final Integer REMOVE_LAST_SPACE_NUMBER = 0;
     private static final Integer MIN_MENU_COUNT = 1;
-    private static final Integer COUNT_INDEX = 1;
-    private static final String SPLIT_ORDER_CHARACTER = ",";
-    private static final String SPLIT_MENU_CHARACTER = "-";
     private static final String INVALID_MESSAGE = "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.";
 
     private ValidationOrder() {
@@ -18,27 +19,41 @@ public class ValidationOrder {
     }
 
     public static void validateOrder(String ordersString) {
-        Stream.of(ordersString.split(SPLIT_ORDER_CHARACTER, REMOVE_LAST_SPACE_NUMBER)).forEach(orderString -> {
-            validateForm(orderString);
-            validateMenuCount(orderString);
+        Stream.of(ordersString.split(SplitTypes.ORDER_REGEX, SplitTypes.REMOVE_SPACE_NUMBER))
+                .map(String::trim)
+                .forEach(orderString -> {
+                    validateForm(orderString);
+                    validateMenuCount(orderString);
+                    validateDuplicateMenu(orderString);
         });
     }
 
     public static void validateForm(String orderString) {
-        Integer menuSize = orderString.split(SPLIT_MENU_CHARACTER, REMOVE_LAST_SPACE_NUMBER).length;
+        Integer menuSize = orderString.split(SplitTypes.MENU_REGEX, SplitTypes.REMOVE_SPACE_NUMBER).length;
         if (menuSize != FORM_SIZE) {
             throw new IllegalArgumentException(INVALID_MESSAGE);
         }
     }
 
     public static void validateMenuCount(String orderString) {
-        String menuCountString = Stream.of(orderString.split(SPLIT_MENU_CHARACTER, REMOVE_LAST_SPACE_NUMBER))
+        String menuCountString = Stream.of(orderString.split(SplitTypes.MENU_REGEX, SplitTypes.REMOVE_SPACE_NUMBER))
                 .toList()
-                .get(COUNT_INDEX);
+                .get(OrderIndices.COUNT.getIndex());
         Integer menuCount = toInteger(menuCountString);
         if (menuCount < MIN_MENU_COUNT) {
             throw new IllegalArgumentException(INVALID_MESSAGE);
         }
+    }
+
+    public static void validateDuplicateMenu(String orderString) {
+        String menuString = Stream.of(orderString.split(SplitTypes.MENU_REGEX, SplitTypes.REMOVE_SPACE_NUMBER))
+                .toList()
+                .get(OrderIndices.MENU.getIndex());
+        if (menuSet.contains(menuString)) {
+            menuSet.clear();
+            throw new IllegalArgumentException(INVALID_MESSAGE);
+        }
+        menuSet.add(menuString);
     }
 
     private static Integer toInteger(String menuCountString) {
