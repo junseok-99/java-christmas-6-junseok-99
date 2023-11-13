a2d100de1e9e93ad8a453772e58ed32142af8d1c
junseok-99 <angec5645@naver.com>
2023-11-13 21:39:05 +0900

    feat: 샴페인 증정 기능 추가

---
+++ src/main/java/christmas/domain/event/presentation/Champagne.java
@@ -0,0 +1,30 @@
+package christmas.domain.event.presentation;
+
+import christmas.domain.customer.Customer;
+import christmas.domain.order.Orders;
+import christmas.repository.menu.Beverage;
+
+public class Champagne implements Presentation {
+
+    private static final Long MIN_PRICE = 120_000L;
+
+    @Override
+    public Boolean isSatisfyCondition(Customer customer) {
+        Orders orders = customer.getOrders();
+        Long beforeDiscountPrice = orders.getBeforeDiscountPrice();
+        if (beforeDiscountPrice >= MIN_PRICE) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void present(Customer customer) {
+        Boolean presentCondition = isSatisfyCondition(customer);
+        Long champagnePrice = null;
+        if (presentCondition) {
+            champagnePrice = NEGATIVE_NUMBER * Beverage.CHAMPAGNE.getPrice();
+        }
+        customer.addBenefit(champagnePrice);
+    }
+}
---
+++ src/main/java/christmas/domain/event/presentation/Presentation.java
@@ -0,0 +1,11 @@
+package christmas.domain.event.presentation;
+
+import christmas.domain.customer.Customer;
+
+public interface Presentation {
+
+    Long NEGATIVE_NUMBER = -1L;
+
+    Boolean isSatisfyCondition(Customer customer);
+    void present(Customer customer);
+}
