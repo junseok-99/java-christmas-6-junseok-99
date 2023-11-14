f39f5bf3d762fceabd61b6bfa99d4ff0880ac2d6
junseok-99 <angec5645@naver.com>
2023-11-14 21:02:15 +0900

    test: 유효한 주문인지 테스트

--- src/test/java/christmas/util/validation/ValidationOrderTest.java
+++ src/test/java/christmas/util/validation/ValidationOrderTest.java
@@ -21,7 +21,7 @@ class ValidationOrderTest {
     @ParameterizedTest
     @DisplayName("유효하지 않은 주문인지 테스트한다.")
     @ValueSource(strings = { "aa-1", "티본스테끼-3", "제로콜라-3, 제로콜라-2", " -1, 타파스-1", "해산물파스타-0", "제로콜라--1"})
-    void validateOrder(String orderString) {
+    void validateInvalidOrder(String orderString) {
         IllegalArgumentException invalidOrderException = assertThrows(IllegalArgumentException.class, () -> {
             ValidationOrder.validateOrder(orderString);
         });
@@ -29,8 +29,18 @@ class ValidationOrderTest {
                 .isEqualTo("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
     }
 
+    @ParameterizedTest
+    @DisplayName("유효한 주문인지 테스트한다.")
+    @ValueSource(strings = { "타파스-1,제로콜라-1", "티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1", "해산물파스타-1,샴페인-1"})
+    void validateOrder(String orderString) {
+        assertDoesNotThrow(() -> {
+            ValidationOrder.validateOrder(orderString);
+        });
+    }
+
     @Test
     void validateForm() {
+
     }
 
     @Test
