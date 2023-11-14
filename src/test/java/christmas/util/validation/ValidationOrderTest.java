f41308b3c1cb262097da87126cda8ca6b66565a5
junseok-99 <angec5645@naver.com>
2023-11-14 21:46:43 +0900

    test: 각 주문의 형식이 올바르지 않은지 테스트 코드 수정

--- src/test/java/christmas/util/validation/ValidationOrderTest.java
+++ src/test/java/christmas/util/validation/ValidationOrderTest.java
@@ -39,10 +39,10 @@ class ValidationOrderTest {
 
     @ParameterizedTest
     @DisplayName("각 주문의 형식이 올바르지 않은지 테스트한다.")
-    @ValueSource(strings = { "타파스*1", "티본스테이크/--1", "바비큐립--1", "초코케이크!2,제로콜라-1", "해산물파스타$1,샴페인+1"})
+    @ValueSource(strings = {"타파스*1", "티본스테이크/--1", "바비큐립--1", "초코케이크@2", "제로콜라@1", "해산물파스타--1"})
     void validateForm(String orderString) {
         IllegalArgumentException invalidFormException = assertThrows(IllegalArgumentException.class, () -> {
-            ValidationOrder.validateOrder(orderString);
+            ValidationOrder.validateForm(orderString);
         });
         assertThat(invalidFormException.getMessage())
                 .isEqualTo("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
@@ -70,8 +70,15 @@ class ValidationOrderTest {
                 .isEqualTo("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
     }
 
-    @Test
-    void validateMenuCountSum() {
+    @ParameterizedTest
+    @DisplayName("총 주문의 개수가 20을 초과하는지 테스트한다.")
+    @ValueSource(strings = { "티본스테이크-15,제로콜라-10", "타파스-10,바비큐립-11,", "초코케이크-21", "해산물파스타-100"})
+    void validateMenuCountSum(String orderString) {
+        IllegalArgumentException invalidDuplicateMenuException = assertThrows(IllegalArgumentException.class, () -> {
+            ValidationOrder.validateOrder(orderString);
+        });
+        assertThat(invalidDuplicateMenuException.getMessage())
+                .isEqualTo("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
     }
 
     @Test
