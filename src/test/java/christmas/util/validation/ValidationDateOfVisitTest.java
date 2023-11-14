9fa4cd4c3f09a4ddb45c4027b67a601f76aa399d
junseok-99 <angec5645@naver.com>
2023-11-14 20:01:33 +0900

    test: 예상 방문 날짜 검증 로직 테스트

---
+++ src/test/java/christmas/util/validation/ValidationDateOfVisitTest.java
@@ -0,0 +1,44 @@
+package christmas.util.validation;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ValidationDateOfVisitTest {
+
+    @ParameterizedTest
+    @DisplayName("유효한 예상 방문 날짜 인지 테스트한다.")
+    @ValueSource(strings = { "a", "b3", "0", "32", "102394845", "1a3", " ", " 3 ", "3  "})
+    void validateDayOfVisit(String dayOfVisit) {
+        IllegalArgumentException invalidDayOfVisitException = assertThrows(IllegalArgumentException.class, () -> {
+            ValidationDayOfVisit.validateDayOfVisit(dayOfVisit);
+        });
+        assertThat(invalidDayOfVisitException.getMessage())
+                .isEqualTo("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+    }
+
+    @ParameterizedTest
+    @DisplayName("예상 방문 날짜가 숫자가 아닌지 테스트한다.")
+    @ValueSource(strings = { "a", "b3", "  3", " ", "뮻ㅇ", "abc", "!!@#$%", " /e/", "3/"})
+    void validateNumeric(String dayOfVisit) {
+        IllegalArgumentException invalidDayOfVisitException = assertThrows(IllegalArgumentException.class, () -> {
+            ValidationDayOfVisit.validateNumeric(dayOfVisit);
+        });
+        assertThat(invalidDayOfVisitException.getMessage())
+                .isEqualTo("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+    }
+
+    @ParameterizedTest
+    @DisplayName("예상 방문 날짜의 범위가 1~31이내가 아닌지 테스트한다.")
+    @ValueSource(strings = { "0", "-1", "-203", "32", "102394845", "100", "9090"})
+    void validateDayRange(String dayOfVisit) {
+        IllegalArgumentException invalidDayOfVisitException = assertThrows(IllegalArgumentException.class, () -> {
+            ValidationDayOfVisit.validateDayRange(dayOfVisit);
+        });
+        assertThat(invalidDayOfVisitException.getMessage())
+                .isEqualTo("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+    }
+}
