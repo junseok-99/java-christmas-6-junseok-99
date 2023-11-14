ca220ada892c594b7eec3762b7db321a26a49650
junseok-99 <angec5645@naver.com>
2023-11-14 21:34:33 +0900

    test: 각 주문의 형식이 올바르지 않은지 테스트

--- src/test/java/christmas/util/validation/ValidationOrderTest.java
+++ src/test/java/christmas/util/validation/ValidationOrderTest.java
@@ -2,7 +2,6 @@ package christmas.util.validation;
 
 import christmas.repository.MenuRepository;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -38,9 +37,15 @@ class ValidationOrderTest {
         });
     }
 
-    @Test
-    void validateForm() {
-
+    @ParameterizedTest
+    @DisplayName("각 주문의 형식이 올바르지 않은지 테스트한다.")
+    @ValueSource(strings = { "타파스*1", "티본스테이크/--1", "바비큐립--1", "초코케이크!2,제로콜라-1", "해산물파스타$1,샴페인+1"})
+    void validateForm(String orderString) {
+        IllegalArgumentException invalidFormException = assertThrows(IllegalArgumentException.class, () -> {
+            ValidationOrder.validateOrder(orderString);
+        });
+        assertThat(invalidFormException.getMessage())
+                .isEqualTo("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
     }
 
     @Test
