33e07a03a461caf6b17dfe93212f871f9b49a7dd
junseok-99 <angec5645@naver.com>
2023-11-14 23:16:17 +0900

    test: 주말 할인이 올바르게 적용되는지 테스트

--- src/test/java/christmas/domain/event/discount/WeekEndTest.java
+++ src/test/java/christmas/domain/event/discount/WeekEndTest.java
@@ -2,6 +2,7 @@ package christmas.domain.event.discount;
 
 import christmas.domain.customer.Customer;
 import christmas.domain.dayofvisit.DayOfVisit;
+import christmas.domain.order.Order;
 import christmas.repository.MenuRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -59,7 +60,18 @@ class WeekEndTest {
         Assertions.assertThat(actualDiscountPrice).isEqualTo(expectedDiscountPrice);
     }
 
-    @Test
-    void discount() {
+    @ParameterizedTest
+    @DisplayName("주말 할인이 올바르게 적용되는지 테스트한다.")
+    @ValueSource(ints = {1, 2, 8, 9, 15, 16, 22, 23, 29, 30})
+    void discount(Integer day) {
+        List<String> orderList = List.of("티본스테이크-3", "바비큐립-2", "해산물파스타-2", "크리스마스파스타-3", "아이스크림-2");
+        Customer customer = new Customer(orderList, new DayOfVisit(day));
+        Long expectedDiscountPrice = 2_023L * 10L;
+
+        weekEnd.discount(customer);
+        Order order = customer.getOrder();
+        Long actualDiscountPrice = order.getBeforeDiscountPrice() - order.getAfterDiscountPrice();
+
+        Assertions.assertThat(actualDiscountPrice).isEqualTo(expectedDiscountPrice);
     }
 }
